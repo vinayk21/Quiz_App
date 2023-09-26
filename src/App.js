8007fb55dc7feee618af3dcbb3340eabd2e43237
vinayk <vickyahir8290@gmail.com>
2023-09-26 18:03:43 +0530

    third change

--- src/App.js
+++ src/App.js
@@ -10,12 +10,11 @@ function App() {
     let res = await axios.get(
       `https://gateway.marvel.com:443/v1/public/comics?ts=1&hash=ae1895a77e42f57a2ff88de40c57a3e6&apikey=99e9936c87d485c889aa77e299bdda7c`
     );
-    useEffect(()=>{
-      console.log("hello users")
-    },[])
-    
     setApidata(res)
   };
+    
+    
+
   useEffect(() => {
     apidata();
   }, []);
